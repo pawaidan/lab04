39196f3ec4d5cb63d6694f25cdf300f8749f8eb1
pawaidan <pawaidan@activation.net>
2018-10-15 11:14:20 -0400

    “setup”

--- irtester.c
+++ irtester.c
@@ -3,21 +3,20 @@
 
 int main(int argc, char *argv[])
 {
-  int i;
   wiringPiSetup () ;
   pinMode(0, INPUT);
-pinMode (1, OUTPUT);
-pinMode (2, OUTPUT);
+  pinMode (1, OUTPUT);
   while(1) {
     printf("Waiting for reset\n");
-    while(digitalRead(0) == 1);
-  printf("Waiting for event\n");
-    digitalWrite (1, LOW);
-
-    while(digitalRead(0) == 0);
-    digitalWrite (1, HIGH);
-    printf("Alarm\n");
-  }
+    while(digitalRead(0) == 1){
+      digitalWrite(1,LOW);
+    }
+    printf("Waiting for event\n");
+    while(digitalRead(0) == 0){
+      digitalWrite (1, HIGH);
+      printf("Alarm\n");
+    }
+ }
   /*NOTREACHED*/
   return 0 ;
 }
